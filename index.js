a57fd66b1d6d5cd1f82f1bd6b4c81aa7fb112e21
tienthinh-fe <thinhhtde150233@fpt.edu.vn>
2023-06-01 13:01:50 +0700

    feat: update new result

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello There!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
