57639bcf61da487711427bcc3e7081901cb67e03
Thinh Hoang <thinh_hoang@datahouse.com>
2023-06-22 14:52:52 +0700

    feat: 12

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello CI/CD! 1'))
+app.get('/', (req, res) => res.send('Hello CI/CD! 12'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
