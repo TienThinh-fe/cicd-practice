28adbd1f33f9639826e836b4559110462f0e44d9
Hoang Tien Thinh <85208341+TienThinh-fe@users.noreply.github.com>
2023-06-23 14:41:01 +0700

    feat: amend combine message (#1)
    Co-authored-by: Thinh Hoang <thinh_hoang@datahouse.com>

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello CI/CD! 12'))
+app.get('/', (req, res) => res.send('Hello CI/CD! 1234'))
 
 app.get('/test', (req, res) => res.send('Test'))
 
